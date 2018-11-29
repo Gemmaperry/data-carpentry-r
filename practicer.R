255ae28ddc3b2543962cd7f3b1253f4e29ac0aae
Gemma <gemma.perry@hdr.mq.edu.au>
2018-11-29 13:49:48 +1100

    session 2

---
+++ practicer.R
@@ -0,0 +1,21 @@
+# dply and tidyr
+library(tidyverse)
+
+interviews <- read_csv("data/SAFI_clean.csv", na ="NULL")
+
+
+select(interviews, village, no_membrs, years_liv)
+select(interviews, village:rooms)
+
+
+# filtering
+filter(interviews, village == "God")
+filter(interviews, 1:nrow(interviews) %in% 1:10)
+
+interviews2 <- filter(interviews, village =="God")
+interviews_god <- select(interviews2, no_membrs, years_liv)
+
+
+interviews_god <- select(filter(interviews, village == "God"), no_membrs, years_liv)
+
+interviews %>% filter(village == "God") %>% select(no_membrs, years_liv)
