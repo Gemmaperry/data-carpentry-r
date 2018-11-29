e1f2bcc74a0b91fe379bf6d0ec7d2976a4f9513e
Gemma <gemma.perry@hdr.mq.edu.au>
2018-11-29 14:19:53 +1100

    grouping and summarising

--- practicer.R
+++ practicer.R
@@ -19,3 +19,14 @@ interviews_god <- select(interviews2, no_membrs, years_liv)
 interviews_god <- select(filter(interviews, village == "God"), no_membrs, years_liv)
 
 interviews %>% filter(village == "God") %>% select(no_membrs, years_liv)
+
+
+# mutate
+
+interviews <- interviews %>% mutate (peope_per_room = no_membrs/rooms)
+
+mean (interviews$no_membrs)
+
+interviews %>% group_by(village) %>%
+  filter (memb_assoc == "yes") %>%
+  summarize (mean_no_membrs = mean(no_membrs))
